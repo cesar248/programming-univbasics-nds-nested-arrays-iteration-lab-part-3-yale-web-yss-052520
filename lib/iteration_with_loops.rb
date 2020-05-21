ff18ca0f3ab6c3ff150986c3721d60240a6a5730
Cesar Lara <cesar.lara@yale.edu>
2020-05-21 23:30:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,18 +4,21 @@ def join_nested_strings(src)
 
 result = []
 row_index = 0
-  while row_index < src.count do
-    element_index = 0
-    while element_index < src[row_index].count do
-      if src[row_index][element_index].is_a? String
-        result.push(src[row_index][element_index])
-      end
-      element_index += 1
-    end
-    row_index += 1
-  end
-
-  return result.join(" ")
-
-
+while row_index < src.count do 
+  element_index = 0
+  while element_index < src[row_index].count do 
+    if src[row_index][element_index].is_a? String
+      result.push(src[row_index][element_index])
+    end 
+    element_index += 1
 end
+row_index += 1 
+end 
+
+
+
+return result.join(" ")
+
+  
+end
+
